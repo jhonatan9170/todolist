468312bf9ad2cf320e465a9b5a5f1ba4607025b4
jhonatan <wily9170@gmail.com>
2021-01-04 20:36:13 -0500

    final

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
+const { static } = require('express');
 const express = require('express');
+const { getDay } = require('./date');
+
 const app = express();
 app.use(express.urlencoded({extended: false}));
 app.set("view engine","ejs");
 app.use(express.json());
+app.use(express.static('public'));
+let items=["Buy Food","Eat Food"];
+let workItems=[];
 
 app.get("/",(req,res)=>{
- var today = new Date();
- var currentDay = today.getDay();
- var day ="";
- switch (currentDay) {
-    case 0:
-    day="Sunday";
-    break;
-    case 1:
-    day="Monday";
-    break;
-    case 2:
-    day="Thursay";
-    break;
-    case 3:
-    day="Wednesday";
-    break;
-    case 4:
-    day="Jueves";
-    break;
-    case 5:
-    day="Friday";
-    break;
-    case 6:
-    day="Saturday";
-    break;
-
-    default:
-    break;
- } 
- 
-res.render("list",{kindOfDay : day});
+ let day = getDay();
+res.render("list",{listTitle : day,listItem :items});
 
 })
 
+app.post("/",(req,res)=>{
+    let item = req.body.newItem;
+    if (req.body.list==="Work") {
+        workItems.push(item);
+        res.redirect("/work")  
+    } else {
+        items.push(item);
+        res.redirect("/")  
+    }
+})
+
+app.get("/work",(req,res)=>{
+    res.render("list",{listTitle:"Work List",listItem:workItems});
+   })
 
 app.listen(3000, () => {
     console.log(`Server on port 30000 `);
